0c63496257d28241144797d3060acf4b8b28a5d1
hnpz22 <131169788+hnpz22@users.noreply.github.com>
2024-05-09 20:40:31 -0500

    ✨ Integrating router service with Legal Cases.
    1. Creating redirection Link and Route in App.tsx
    2. Creating function at legalCase.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { Clients } from "./pages/clients";
 import { Lawyers } from "./pages/lawyers";
@@ -14,6 +15,32 @@ function LegalManagementApp() {
           {<Route path="*" element={<NoMatch />} />}
         </Routes>
       </Router>
+=======
+import { Link, Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import { LegalCase } from "./pages/legalCase";
+import { Login } from "./pages/login";
+
+function LegalManagementApp() {
+  return (
+    <>
+    <div>LegalManagementApp</div>
+    <Router>
+      <nav style={{ margin: 10 }}>
+      <Link to="/" style={{ padding: 5 }}>
+          Login
+        </Link>
+        <Link to="/legalCase" style={{ padding: 5 }}>
+          Legal Case Page
+        </Link>
+      </nav>
+        <Routes>
+          {<Route path="/" element={<Login />} />}
+          {/* <Route path="/about" element={<About />} /> */}
+          {/* <Route path="*" element={<NoMatch />} /> */}
+          {<Route path="/legalCase" element={<LegalCase />} />}
+        </Routes>
+    </Router>
+>>>>>>> Stashed changes
     </>
   );
 }
---
+++ src/pages/legalCase.tsx
@@ -0,0 +1,7 @@
+export function LegalCase (){
+    return (
+        <div>
+            <h2>Legal Cases Page!!</h2>
+        </div>
+    )
+}
