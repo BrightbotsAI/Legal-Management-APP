a0cc7c9b600357bcf31e4cf6346e4a0324cac2cd
hnpz22 <131169788+hnpz22@users.noreply.github.com>
2024-05-09 20:43:23 -0500

    ✨ Integrating router service with Legal Cases.
    1. Creating redirection Link and Route in App.tsx
    2. Creating function at legalCase.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,3 @@
-<<<<<<< Updated upstream
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import { Clients } from "./pages/clients";
-import { Lawyers } from "./pages/lawyers";
-import { NoMatch } from "./pages/noMatch";
-import { Home } from "./pages/home";
-function LegalManagementApp() {
-  return (
-    <>
-      <Router>
-        <Routes>
-          {<Route path="/" element={<Home />} />}
-          {<Route path="/clients" element={<Clients />} />}
-          {<Route path="/lawyers" element={<Lawyers />} />}
-          {<Route path="*" element={<NoMatch />} />}
-        </Routes>
-      </Router>
-=======
 import { Link, Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import { LegalCase } from "./pages/legalCase";
 import { Login } from "./pages/login";
@@ -40,9 +22,15 @@ function LegalManagementApp() {
           {<Route path="/legalCase" element={<LegalCase />} />}
         </Routes>
     </Router>
->>>>>>> Stashed changes
     </>
   );
 }
 
 export default LegalManagementApp;
+
+
+
+
+
+
+
