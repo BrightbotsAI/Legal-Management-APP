027179e66658557aadbfd3d30ae31ce15bffd7fa
Cristian Medina <cristianc@brightbots.co>
2024-05-10 10:32:18 -0500

    ✨ New components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,28 @@
-import { Link, Route, BrowserRouter as Router, Routes} from "react-router-dom";
-import { LegalCase } from "./pages/legalCase";
-import { Login } from "./pages/login";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { NavBar } from "./components";
+import { Clients } from "./pages/Clients";
+import { Lawyers } from "./pages/Lawyers";
+import { LegalCase } from "./pages/LegalCase";
+import { LegalDocuments } from "./pages/LegalDocuments";
+import { Login } from "./pages/Login";
+import { NoMatch } from "./pages/NoMatch";
+import { Precedents } from "./pages/Precedents";
 
 function LegalManagementApp() {
   return (
-    <>
-    <div>LegalManagementApp</div>
     <Router>
-      <nav style={{ margin: 10 }}>
-      <Link to="/" style={{ padding: 5 }}>
-          Login
-        </Link>
-        <Link to="/legalCase" style={{ padding: 5 }}>
-          Legal Case Page
-        </Link>
-      </nav>
-        <Routes>
-          {<Route path="/" element={<Login />} />}
-          {/* <Route path="/about" element={<About />} /> */}
-          {/* <Route path="*" element={<NoMatch />} /> */}
-          {<Route path="/legalCase" element={<LegalCase />} />}
-        </Routes>
+      <NavBar />
+      <Routes>
+        {<Route path="/" element={<Login />} />}
+        <Route path="/clients" element={<Clients />} />
+        {<Route path="/lawyers" element={<Lawyers />} />}
+        {<Route path="/legalCase" element={<LegalCase />} />}
+        {<Route path="/legalDocuments" element={<LegalDocuments />} />}
+        {<Route path="/precedents" element={<Precedents />} />}
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
     </Router>
-    </>
   );
 }
 
 export default LegalManagementApp;
-
-
-
-
-
-
-
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,58 @@
+import { NavLink } from "react-router-dom";
+
+export const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          Legal Management App
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ">
+            <li className="nav-item me-2">
+              <NavLink className="nav-link" to="/">
+                Login
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link " to="/clients">
+                Clients
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link " to="/lawyers">
+                Lawyers
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link " to="/legalCase">
+                LegalCase
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link " to="/legalDocuments">
+                LegalDocuments
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link " to="/precedents">
+                Precedents
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
