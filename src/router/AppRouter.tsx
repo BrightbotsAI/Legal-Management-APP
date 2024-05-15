d01130ed64f33462f14e896c94d12a12429832b0
hnpz22 <131169788+hnpz22@users.noreply.github.com>
2024-05-15 07:45:17 -0500

    ✨ Legal Cases Form created

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-import { Clients } from "../pages/Clients";
-import { Lawyers } from "../pages/Lawyers";
+import { Clients } from "../pages/clients";
+import { Lawyers } from "../pages/lawyers";
 import { LegalDocuments } from "../pages/LegalDocuments";
-import { Login } from "../pages/Login";
+import { Login } from "../pages/login";
 import { NoMatch } from "../pages/noMatch";
 import { Precedents } from "../pages/Precedents";
-import { LegalCase } from "../pages/LegalCase";
+import { LegalCase } from "../pages/legalCase";
 
 export const AppRouter = () => {
   return (
