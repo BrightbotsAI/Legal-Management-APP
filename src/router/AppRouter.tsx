e176b353d46a18e619731fef97c4283bfe08ddfc
Cristian Medina <cristianc@brightbots.co>
2024-05-15 10:13:40 -0500

    🚧 Update Router

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-import { Clients } from "../pages/Clients";
-import { Lawyers } from "../pages/Lawyers";
 import { LegalDocuments } from "../pages/LegalDocuments";
-import { Login } from "../pages/Login";
-import { NoMatch } from "../pages/noMatch";
+import { NoMatch } from "../pages/NoMatch";
 import { Precedents } from "../pages/Precedents";
-import { LegalCase } from "../pages/LegalCase";
+import { Clients } from "../pages/clients";
+import { Lawyers } from "../pages/lawyers";
+import { LegalCase } from "../pages/legalCase";
+import { Login } from "../pages/login";
 
 export const AppRouter = () => {
   return (
